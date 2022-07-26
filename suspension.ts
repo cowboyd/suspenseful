c2716c30daff852ec3cbfeadc897e04feae8506c
Charles Lowell <cowboyd@frontside.com>
2022-07-26 00:55:50 -0500

    normalize context passing

--- suspension.ts
+++ suspension.ts
@@ -1,33 +1,61 @@
 import { evaluate, shift, reset, Computation } from "./deps.ts";
+import { Future } from './future.ts';
 
-function* operation<T>(block: (resume: (result: T) => void) => Computation<T>): Computation<T> {
-  return yield* shift<T>(function*($return) {
-    let context: Context = {
-      *unsuspend() {},
+export interface Task<T> extends Future<T> {
+  halt(): Future<void>;
+}
+
+export function run<T>(block: () => Computation<T>): Future<T> {
+  return Future.eval(function*() {
+    let initial: Context = {
+      *unsuspend() {}
     };
-
-    let resume = (value: T) => evaluate(function*() {
-      yield* context.unsuspend();
-      // yield* $return(value)(parent)
-
-      $return(value);
-    });
-
-    return yield* reduce(() => block(resume), context);
+    return yield* reduce(block, initial);
   });
 }
 
+function* fn<T>(block: (resume: (result: T) => void) => Computation<T>): Computation<T> {
+  return yield* shift<T>(function*(k) {
+    return function*(parent: Context) {
+      let child: Context = {
+        *unsuspend() {}
+      };
+      let resume = (value: T) => evaluate(function*() {
+        yield* child.unsuspend();
+        yield* k(value)(parent)
+      });
+
+      return yield* reduce(() => block(resume), child);
+    }
+
+  });
+}
+
+
+//TODO: how to unwind context through multiple effects? Can it control the return value?
+//TODO: context.expect() / context.join();
 export function* effect<T>(body: (provide: (value: T) => Computation<void>) => Computation<void>): Computation<T> {
 
-  return yield* shift<T>(function*(k) {
+  return yield* shift<T>(function*(resume) {
+    return function*(parent: Context) {
 
-    return function*(context: Context) {
       function* provide(value: T): Computation<void> {
-        yield* k(value)(context);
+        yield* shift<void>(function*(resolve, reject) {
+          return function*(context: Context) {
+            console.log('resume child');
+            try {
+              yield* resume(value)(parent);
+            //yield* parent.expect();
+              yield* resolve()(context);
+              console.log('resume child done');
+            } catch (error) {
+              yield* reject(error)(context);
+            }
+            console.log('finish provide');
+          };
+        });
       }
-      console.log('start reducing the body');
-      yield* reduce(() => body(provide), context);
-      console.log('done reducing the body')
+      yield* reduce(() => body(provide), { *unsuspend() {} });
     };
   });
 }
@@ -46,7 +74,6 @@ function* reduce<T, TContext>(block: () => Computation<T>, context: TContext): C
   return yield* start(context);
 }
 
-
 export function* suspend() {
   yield* shift<void>(function*(k) {
     return function*(context: Context) {
@@ -56,11 +83,11 @@ export function* suspend() {
 }
 
 export function sleep(duration: number): Computation<void> {
-  return operation(function* (resume) {
+  return fn(function* (resume) {
     yield* effect(function*(provide) {
       let interval = setInterval(() => console.log('snore', 200));
       try {
-        yield* provide();
+        return yield* provide();
       } finally {
         clearInterval(interval);
       }
@@ -68,9 +95,7 @@ export function sleep(duration: number): Computation<void> {
 
     let timeout = setTimeout(resume, duration);
     try {
-      console.log('will suspend');
       yield* suspend();
-      console.log('did suspend');
     } finally {
       console.log('timeout cleared');
       clearTimeout(timeout);
@@ -78,10 +103,10 @@ export function sleep(duration: number): Computation<void> {
   })
 }
 
-evaluate(function*() {
+await run(function*() {
   console.log('yawn');
-  yield* sleep(1500);
+  yield* sleep(2500);
   console.log('what?');
-  yield* sleep(1500);
+  yield* sleep(2500);
   console.log('done');
 })
